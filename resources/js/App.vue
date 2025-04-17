84bfa16cf22574cf53dd432cd739df55fc0b64fc
Daniel Alvarez Gonçalves <alvarezgonca@gmail.com>
2025-04-16 23:21:08 -0300

    Feat: adiciona o componente App.vue simulando o layout com <router-view />

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <header>
+        <h1>Meu layout com Vue</h1>
+      </header>
+
+      <router-view />
+
+      <footer>
+        <p>© 2025 - Todos os direitos reservados</p>
+      </footer>
+    </div>
+  </template>
